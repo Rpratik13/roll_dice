a4136e40a4efadac7e15b2a510d485ae126e4993
Pratik Rajbhandari <r.pratik013@gmail.com>
2024-05-01 17:02:01 +0545

    Complete dice roll app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:roll_dice/dice_roller.dart';
+
+const alignmentStart = Alignment.topLeft;
+const alignmentEnd = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(
+    this.color1,
+    this.color2, {
+    super.key,
+  });
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: alignmentStart,
+          end: alignmentEnd,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
